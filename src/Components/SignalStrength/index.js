e2c7b54219bdb6d12bcaec4daafa7cf20bd8ac35
Rajja Farhan <rajjafarhan2@gmail.com>
2024-02-19 17:03:07 +0500

    updations of responsiveness by rajja2

---
+++ src/Components/SignalStrength/index.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+
+const Signalstrength = ({ batteryPercentage }) => {
+  const [signalBars, setSignalBars] = useState([]);
+
+  useEffect(() => {
+    const calculateSignalBars = () => {
+      let bars = [];
+
+      if (batteryPercentage <= 20 && batteryPercentage>0 || batteryPercentage === 0) {
+        bars = ['black', 'gray', 'gray', 'gray', 'gray'];
+      } else if (batteryPercentage < 40 && batteryPercentage > 20) {
+        bars = ['yellow', 'yellow', 'gray', 'gray', 'gray'];
+      } else if (batteryPercentage < 60 ) {
+        bars = ['green', 'green', 'green', 'gray', 'gray'];
+      } else if (batteryPercentage === 100) {
+        bars = ['green', 'green', 'green', 'green', 'green'];
+      } else {
+        bars = ['gray', 'gray', 'gray', 'gray', 'gray'];
+      }
+
+      setSignalBars(bars);
+    };
+
+    calculateSignalBars();
+  }, [batteryPercentage]);
+
+  return (
+    <div className="flex flex-row items-end gap-1">
+      {signalBars.map((color, index) => (
+        <div
+          key={index}
+          className={`w-2 bg-${color}-600 rounded-md`}
+          style={{ height: `${index * 5 + 10}px` }}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Signalstrength;
